f8db8c5dc6d962bea3265d57e506e5428bd2381a
yr1404 <yash06765@gmail.com>
2023-06-02 18:45:05 +0530

    File structure defined and About section created

---
+++ src/components/Hero/Hero.jsx
@@ -0,0 +1,13 @@
+import './Hero.css'
+
+const Hero = () => {
+    return (
+        <div className="container">
+          <div className="hero">
+            {/* Your hero section content goes here */}
+          </div>
+        </div>
+      )
+}
+
+export default Hero
