86b2e8f7035e267a0844ba88f19c8e73422ce5c0
kahoragachau <joelkahoragachau6@gmail.com>
2023-06-05 15:53:40 +0300

    -feat: ✨dd Navbar Items

--- src/components/Nav/Navbar.jsx
+++ src/components/Nav/Navbar.jsx
@@ -3,7 +3,21 @@ import './Navbar.css';
 const Navbar = () => {
   return (
     <>
-      <h1>It Works!!</h1>
+      <div className="nav-container">
+        <div className="logo">
+          Triv
+        </div>
+        <ul>
+          <li><a href="home">Home</a></li>
+          <li><a href="about">About</a></li>
+          <li><a href="contact">Contact</a></li>
+          <li>
+            <button>
+              sign up
+            </button>
+          </li>
+        </ul>
+      </div>
     </>
   )
 }
