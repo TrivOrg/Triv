d82f29a46b8fb13aaac536e6785a6afa21c58221
kahoragachau <joelkahoragachau6@gmail.com>
2023-06-06 21:38:34 +0300

    💄style: add font color to the logo

--- src/components/Nav/Navbar.jsx
+++ src/components/Nav/Navbar.jsx
@@ -11,12 +11,12 @@ const Navbar = () => {
           <li><a href="home">Home</a></li>
           <li><a href="about">About</a></li>
           <li><a href="contact">Contact</a></li>
-          <li>
-            <button>
-              sign up
-            </button>
-          </li>
         </ul>
+        <div className="button">
+          <button className="sign-up">
+            Sign Up
+          </button>
+        </div>
       </div>
     </>
   )
