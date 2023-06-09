442e1a27961a1323c80996183817e77767da83b5
yr1404 <yash06765@gmail.com>
2023-06-09 14:54:28 +0530

    social media icons added

--- src/components/Contacts/contacts.jsx
+++ src/components/Contacts/contacts.jsx
@@ -10,17 +10,17 @@ const Contacts = () => {
                         <h3 className="t3">OUR SOCIALS</h3>
                         <h3 className="t3">Follow Us On Social Media</h3>
                         <div className="social-icons">
-                            <a href="#">
-                            <ion-icon class="icons" name="logo-instagram"></ion-icon>
+                            <a href="#insta">
+                                <ion-icon name="logo-instagram"></ion-icon>
                             </a>
-                            <a href="#"> 
-                            <ion-icon name="logo-facebook"></ion-icon>
+                            <a href="#fb">
+                                <ion-icon name="logo-facebook"></ion-icon>
                             </a>
-                            <a href="#">
-                            <ion-icon name="logo-github"></ion-icon>
+                            <a href="#github">
+                                <ion-icon name="logo-github"></ion-icon>
                             </a>
-                            <a href="#">
-                            <ion-icon name="logo-github"></ion-icon>
+                            <a href="#twitter">
+                                <ion-icon name="logo-twitter"></ion-icon>
                             </a>
                         </div>
                     </div>
