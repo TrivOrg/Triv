b8d7fd597681c6e93e6bef664598035a5efcb3fd
kahoragachau <joelkahoragachau6@gmail.com>
2023-06-13 19:16:23 +0300

    🚩fix: Restructure the project && Add Home Component

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,3 @@
-import Hero from './components/Hero/Hero'
-import About from './components/About/About'
-import Navbar from './components/Nav/Navbar'
-import Contacts from './components/Contacts/contacts'
 import Home from './components/Home/Home'
 import { BrowserRouter ,Route, Routes } from 'react-router-dom'
 
