46a5a9fea040cbca5641d606e663d30ffa0f5b2b
kahoragachau <joelkahoragachau6@gmail.com>
2023-06-13 17:53:37 +0300

    🐛fix: Hero sub-text style

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -8,7 +8,7 @@ const Hero = () => {
             {/* Your hero section content goes here */}
             <div className="hero-top-text">
               <p>Play, Create, Rate Quizzess in Areas You need proper understanding by practising</p>
-              <p>Practices Makes Perfect</p>
+              <p className="sub-text">Practices Makes Perfect</p>
               <div className="get-started">
                 <input type="text" placeholder='johndoe@gmail.com' />
                 <button> Get Started</button>
