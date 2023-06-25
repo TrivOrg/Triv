aa4e1b9a500b96050ca7809f0fb63cf66ab8a365
kahoragachau <joelkahoragachau6@gmail.com>
2023-06-25 21:28:05 +0300

    🎨style: Format code using Prettier

--- src/components/Nav/Navbar.jsx
+++ src/components/Nav/Navbar.jsx
@@ -1,27 +1,31 @@
-import './Navbar.css';
-import { Link, useNavigate } from 'react-router-dom';
+import "./Navbar.css";
+import { Link, useNavigate } from "react-router-dom";
 
 const Navbar = () => {
   const navigate = useNavigate();
   return (
     <>
       <div className="nav-container">
-        <div className="logo">
-          Triv
-        </div>
+        <div className="logo">Triv</div>
         <ul>
-          <li><Link to="/">Home</Link></li>
-          <li><a href="#about">About</a></li>
-          <li><a href="#contacts">Contact</a></li>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <a href="#about">About</a>
+          </li>
+          <li>
+            <a href="#contacts">Contact</a>
+          </li>
         </ul>
         <div className="button">
-          <button className="sign-up" onClick={() => navigate('/signup')}>
+          <button className="sign-up" onClick={() => navigate("/signup")}>
             Sign Up
           </button>
         </div>
       </div>
     </>
-  )
-}
+  );
+};
 
 export default Navbar;
